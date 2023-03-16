8386b82dac19fdaeb8e0f796bae23af244e185ce
jaume t <jaumetruyols@paucasesnovescifp.cat>
2023-03-16 02:38:06 +0100

    carrrusel arreglado con los destacados y info de alojamiento al dar click en la foto

--- src/componentes/Carrusel.jsx
+++ src/componentes/Carrusel.jsx
@@ -1,62 +1,54 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import {Button} from "react-bootstrap";
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function Carrusel() {
-    const [index, setIndex] = useState(0);
     const [imatges, setImatges] = useState([]);
-    const length = imatges.length;
-
-    const handlePrevious = () => {
-        const newIndex = index - 1;
-        setIndex(newIndex < 0 ? length - 1 : newIndex);
-    };
-
-    const handleNext = () => {
-        const newIndex = index + 1;
-        setIndex(newIndex >= length ? 0 : newIndex);
-    };
+    const [allotjamentsDestacats, setAllotjamentsDestacats] = useState([]);
 
     useEffect(() => {
-        axios
-            .get("http://www.etvtauladesfons.com/api/fotografies")
-            .then((response) => {
-                setImatges(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+        const fetchData = async () => {
+            try {
+                const responseAllotjaments = await axios.get('http://www.etvtauladesfons.com/api/allotjaments');
+                const allotjamentsDestacats = responseAllotjaments.data.data.filter((allotjament) => allotjament.DESTACAT === 1);
+                setAllotjamentsDestacats(allotjamentsDestacats);
 
-        const interval = setInterval(() => {
-            handlePrevious();
-        }, 2000);
+                const responseFotografies = await axios.get('http://www.etvtauladesfons.com/api/fotografies');
+                const fotografiesDestacades = responseFotografies.data.filter((foto) =>
+                    allotjamentsDestacats.some((allotjament) => allotjament.ID_ALLOTJAMENT === foto.FK_ID_ALLOTJAMENT)
+                );
+                setImatges(fotografiesDestacades);
+            } catch (error) {
+                console.error('Error al obtener los datos', error);
+            }
+        };
+        fetchData();
+    }, []);
 
-        return () => clearInterval(interval);
-    });
+    const getNomAllotjament = (idAllotjament) => {
+        const allotjament = allotjamentsDestacats.find((a) => a.ID_ALLOTJAMENT === idAllotjament);
+        return allotjament ? allotjament.NOM_COMERCIAL : '';
+    };
 
     return (
-
-        <div className="carousel" style={{display: "flex", alignItems: "center", alignContent: "center"}}>
-            <div>
-                <Button variant="outline-secondary"
-                        style={{position: "absolute", zIndex: "100", left: "20px", top: 100, fontSize: "100px"}}
-                        onClick={handlePrevious}
-                > {"•"}
-                </Button>
-                <Button variant="outline-secondary"
-                        style={{position: "absolute", zIndex: "100", left: "456px", top: 100, fontSize: "100px"}}
-                        onClick={handleNext}> {"•"}
-                </Button>
-                {imatges.length > 0 && (
-                    <img style={{margin: "20px", height: "350px", width: "500px", maxWidth: "500px"}}
-                        src={imatges[index].URL}
-                        alt={imatges[index].DESCRIPCIO}
-                    />
-                )}
-            </div>
-        </div>
-
+        <Carousel>
+            {imatges.map((imatge) => (
+                <Carousel.Item key={imatge.ID_FOTO}>
+                    <Link to={`/allotjament/${imatge.FK_ID_ALLOTJAMENT}`}>
+                        <img
+                            className="d-block w-100"
+                            src={imatge.URL}
+                            alt={imatge.DESCRIPCIO}
+                            style={{ maxHeight: '50vh', objectFit: 'contain' }}
+                        />
+                    </Link>
+                    <Carousel.Caption>
+                        <h3>{getNomAllotjament(imatge.FK_ID_ALLOTJAMENT)}</h3>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            ))}
+        </Carousel>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
